7fb4fa625650f8f8aca74a2887a44133a7ae9249
Osvaldo <torquati.osvaldo@gmail.com>
2022-09-26 12:39:19 -0300

    Atualização 220926
    Resolvido problema 1
    Iniciado solução do problema 2

---
+++ Problem2/problem2.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} : String to be tested for validity
+ * @return {boolean} : Retruns true if the string is vald else false
+ */
+var isValid = function(s){
+let a=[];
+// Verifica p comprimento de s (de 1 a 104)
+if (s.length<1 || s.length>104) return false;
+
+// Verificar se s contem caracteres válidos
+
+// Verificar a ordem dos caracteres
+// definir tipo de caracter
+// verificar proximo caracter
+
+return true;
+};
+
+let s=['(',')','{','}','[',']'];
+if (isValid(s)) console.log("valid");
+else console.log("invalid");
