c00926d3118671a9544f67e9637b0b15b3c233bd
Osvaldo <torquati.osvaldo@gmail.com>
2022-09-26 21:35:18 -0300

    Update problem2.js
    Melhorias na solução

--- Problem2/problem2.js
+++ Problem2/problem2.js
@@ -4,12 +4,20 @@
  */
 var isValid = function(s){
 let a=[];
+console.log(s.length);
 // Verifica p comprimento de s (de 1 a 104)
-if (s.length<1 || s.length>104) return false;
+if ((s.length<1)||(s.length>104)) return false;
 
 // Verificar se s contem caracteres válidos
-
+for(i=0;i<=s.length-1;i++){
+  if (a==(s=='('||s==')'||s=='{'||s=='}'||s=='['||s==']')){
+    a+=s;
+  }else{
+    return false;
+  }
+}
 // Verificar a ordem dos caracteres
+
 // definir tipo de caracter
 // verificar proximo caracter
 
