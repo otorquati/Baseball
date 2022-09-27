0b72042b359ad63c9014bff08749357dff2f33d1
Osvaldo Torquati <62728159+otorquati@users.noreply.github.com>
2022-09-27 01:42:08 -0300

    Update problem2.js
    Final version

--- Problem2/problem2.js
+++ Problem2/problem2.js
@@ -4,26 +4,65 @@
  */
 var isValid = function(s){
 let a=[];
+let result=false;
 console.log(s.length);
+console.log(`s= ${s}`);
 // Verifica p comprimento de s (de 1 a 104)
 if ((s.length<1)||(s.length>104)) return false;
 
 // Verificar se s contem caracteres válidos
 for(i=0;i<=s.length-1;i++){
-  if (a==(s=='('||s==')'||s=='{'||s=='}'||s=='['||s==']')){
-    a+=s;
+  if ((s[i]=='(')||(s[i]==')')||(s[i]=='{')||(s[i]=='}')||(s[i]=='[')||(s[i]==']')){
+    a.push(s[i]);
+    console.log(`a = ${a}`);
   }else{
-    return false;
+    return result;
   }
 }
+var cp=0;
+var cc=0;
+var cch=0;
+a=[];
 // Verificar a ordem dos caracteres
-
-// definir tipo de caracter
-// verificar proximo caracter
-
-return true;
+for( i=0;i<=(s.length); i++){
+a=s[i]
+  switch (a){
+    case '(':
+      if(cp==0){
+        cp+=1;
+      } else {
+        cp=cp+cch+cc+1;
+      }
+      break;
+    case '{':
+      if(cch==0){
+        cch+=1;
+      } else {
+        cch=cp+cch+cc+1;
+      }
+      break;
+    case '[':
+      if(cc==0){
+        cc+=1;
+      } else {
+        cc=cp+cch+cc+1;
+      }
+      break;
+    case ')':
+      cp-=1;
+      break;
+    case '}':
+      cch-=1;
+      break;
+    case ']':
+      cc-=1;    
+  }
+console.log(`cp= ${cp} cch= ${cch} cc= ${cc}`);
+}
+if ((cc==0) && (cch==0) && (cp==0)){result=true};
+return result;
 };
 
-let s=['(',')','{','}','[',']'];
+let s=['(','}','{',']','[',')'];
 if (isValid(s)) console.log("valid");
 else console.log("invalid");
